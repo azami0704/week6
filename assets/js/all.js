8408f86bc62408695c9b8662f260d05bb835e143
azami0704 <doggy0704@gmail.com>
2022-08-30 15:29:07 +0800

    Update 2022-08-30T07:29:02.144Z

---
+++ assets/js/all.js
@@ -0,0 +1,20 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+}); // swiper
+
+var swiper = new Swiper(".swiper", {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+//# sourceMappingURL=all.js.map
